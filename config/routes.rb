7d7f677f7f4fca1338349eb998859a00eb40efab
yui0308ks <kazuyu03081@gmail.com>
2020-01-06 17:47:02 +0900

    reset.scssの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'messages/index'
   get 'message/index'
   root "messages#index"
 end
--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class MessageControllerTest < ActionDispatch::IntegrationTest
+class MessagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get message_index_url
+    get messages_index_url
     assert_response :success
   end
 
